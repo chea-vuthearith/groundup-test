b836029f35bcec9541c229ecffe32f0dd39d34fc
CheaVuthearith <cheavuthearith0@gmail.com>
2025-03-18 01:26:46 +0700

    fix: spectrogram not updating

--- src/app/app/alerts/_components/sound-charts.tsx
+++ src/app/app/alerts/_components/sound-charts.tsx
@@ -1,51 +1,37 @@
 "use client";
-import React from "react";
-import WaveSurfer from "wavesurfer.js";
+import WaveSurferReact from "@wavesurfer/react";
+import type WaveSurfer from "wavesurfer.js";
 import Spectrogram from "wavesurfer.js/dist/plugins/spectrogram.esm.js";
 type Props = {
   title: string;
   audioUrl: string;
 };
 const SoundCharts = (props: Props) => {
-  const containerRef = React.useRef<HTMLDivElement | null>(null);
-  const waveSurferRef = React.useRef<WaveSurfer | null>(null);
-
-  const wavesurferOptions = React.useMemo(
-    () => ({
-      url: props.audioUrl,
-      getPlugins: () => [
-        Spectrogram.create({
-          labels: true,
-          frequencyMax: 8192,
-          labelsBackground: "rgba(0, 0, 0, 0.1)",
-          height: 200,
-        }),
-      ],
-      plugins: [],
-    }),
-    [props.audioUrl],
-  );
-
-  React.useEffect(() => {
-    if (!waveSurferRef.current) {
-      waveSurferRef.current = WaveSurfer.create({
-        ...wavesurferOptions,
-        mediaControls: true,
-        container: containerRef.current as HTMLElement,
-        plugins: wavesurferOptions.getPlugins(),
-      });
-    }
-    // waveSurferRef.current?.destroy();
-  }, []);
+  const onInit = (wavesurfer: WaveSurfer) => {
+    wavesurfer.registerPlugin(
+      Spectrogram.create({
+        labels: true,
+        frequencyMax: 8192,
+        labelsBackground: "rgba(0, 0, 0, 0.1)",
+        height: 200,
+      }),
+    );
+  };
 
   return (
-    <div className="flex grow flex-col gap-y-2">
-      <h2 className="text-xl">{props.title}</h2>
-      <div
-        ref={containerRef}
-        className="[&_div]:flex [&_div]:flex-col-reverse [&_div]:gap-y-4"
-      />
-    </div>
+    <>
+      <div className="relative flex grow flex-col gap-y-2">
+        <h2 className="text-xl">{props.title}</h2>
+        <div className="[&_div]:flex [&_div]:flex-col-reverse [&_div]:gap-y-4">
+          <WaveSurferReact
+            waveColor="#526cfe"
+            onInit={onInit}
+            mediaControls
+            url={props.audioUrl}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
